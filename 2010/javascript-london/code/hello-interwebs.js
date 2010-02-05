ac952cda920f191a3afd25390af2020519b4db4f
Craig R Webster <craig@barkingiguana.com>
2010-02-05 12:10:50 +0000

    Flatten the talk namespace.

--- 2010/javascript-london/code/hello-interwebs.js
+++
@@ -1,9 +0,0 @@
-var sys  = require("sys"),
-    http = require("http");
-http.createServer(function(request, response) {
-  var headers = { "Content-Type": "text/plain" };
-  response.sendHeader(200, headers);
-  response.sendBody("Hello, World!\n");
-  response.finish();
-}).listen(8000);
-sys.puts("> Running at http://127.0.0.1:8000/");
